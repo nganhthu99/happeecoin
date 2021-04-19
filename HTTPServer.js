f30e48c0f8035e160493094357f577663fd55197
Thu Anh Nguyen <nganhthu99@gmail.com>
2021-04-19 10:44:12 +0700

    V1 - update http server

--- HTTPServer.js
+++ HTTPServer.js
@@ -1,30 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { getSockets } = require("./P2PServer");
-const blockchain = require('./Global').blockchain
+const { getSockets, connectToPeers } = require("./P2PServer");
+const { getBlockchain, generateNextBlock } = require("./Blockchain");
 
-function initHttpServer(httpPort) {
+const initHttpServer = (httpPort) => {
     const app = express()
 
     app.use(bodyParser.json())
 
     app.get('/blocks', (req, res) => {
-        res.send(blockchain.getBlockchain())
+        res.send(getBlockchain())
     })
 
-    // app.post('/mineBlock', (req, res) => {
-    //     const newBlock = blockchain.generateNextBlock(req.body.data)
-    //     res.send(newBlock)
-    // })
+    app.post('/mineBlock', (req, res) => {
+        const newBlock = generateNextBlock(req.body.data)
+        res.send(newBlock)
+    })
 
     app.get('/peers', (req, res) => {
         res.send(getSockets().map((s) => s._socket.remoteAddress + ':' + s._socket.remotePort))
     })
 
-    // app.post('/addPeer', (req, res) => {
-    //     connectToPeers(req.body.peer);
-    //     res.send();
-    // });
+    app.post('/addPeer', (req, res) => {
+        connectToPeers(req.body.peer)
+        res.send();
+    })
 
     app.listen(httpPort, () => {
         console.log('Listening HTTP on port: ' + httpPort);

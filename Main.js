1b7a320e7e2a738614b3d8a79348f7445c86ab4e
Thu Anh Nguyen <nganhthu99@gmail.com>
2021-05-05 17:22:45 +0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,8 @@
-const { initBlockchain } = require("./Blockchain");
-const { connectToPeers } = require("./P2PServer");
-const { initWallet } = require("./Wallet");
-const { initHttpServer } = require('./HTTPServer')
-const { initP2PServer } = require('./P2PServer')
+const { initBlockchain } = require("./src/Blockchain");
+const { connectToPeers } = require("./src/P2PServer");
+const { initWallet } = require("./src/Wallet");
+const { initHttpServer } = require('./src/HTTPServer')
+const { initP2PServer } = require('./src/P2PServer')
 
 initWallet()
 initBlockchain()
